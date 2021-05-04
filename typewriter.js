f1a80cc4ddc7dec8a1f9635f84c589777aa69f38
Claire Devlin <claire.devlin@hotmail.com>
2021-05-04 13:31:58 -0400

    implemented typewriter function

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+
+const sentence = "hello there from lighthouse labs";
+let i = 0;
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+    if (i === sentence.length - 1) {
+      process.stdout.write("\n");
+    }
+  }, 50 * i);
+}
